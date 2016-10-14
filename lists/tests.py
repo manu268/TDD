fb85a0462ccd6e50233ad31a26912cbcac7fcccb
luckyguy <luckyguy268@gmail.com>
2016-10-15 07:16:14 +0800

    unit test pas

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from lists import views 
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')  
         self.assertEqual(found.func, views.home_page)  
+        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        response = views.home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))  #
