9ee6761b471ed9a7fec7707018c87df13ac71c98
luckyguy <luckyguy268@gmail.com>
2016-10-15 13:36:30 +0800

    Complete Chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -41,6 +41,7 @@ class NewVisitorTest(unittest.TestCase):  #1
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        self.browser.get('http://localhost:8000')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
     
         # There is still a text box inviting her to add another item. She
@@ -49,8 +50,8 @@ class NewVisitorTest(unittest.TestCase):  #1
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-    
         # The page updates again, and now shows both items on her list
+        self.browser.get('http://localhost:8000')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
     
