7779cf1f3c652a91808f9d91bca2280200280d52
luckyguy <luckyguy268@gmail.com>
2016-10-16 12:31:33 +0800

    Added db folder

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,16 +1,18 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+import time,os
 
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         #self.browser = webdriver.Firefox(executable_path="C:\Python27\wires.exe")
         self.browser = webdriver.Firefox(executable_path="C:\Python27\wires.exe")
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        #time.sleep(20)
+        print(os.system('tskill plugin-container'))
         self.browser.quit()
 
 
@@ -45,7 +47,7 @@ class NewVisitorTest(LiveServerTestCase):
         # and now the page lists "1: Buy peacock feathers" as an item in a
         # to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(2)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -56,7 +58,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(2)
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -65,12 +67,14 @@ class NewVisitorTest(LiveServerTestCase):
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
+        print("Killing Task",os.system('tskill plugin-container'))
         self.browser.quit()
         self.browser = webdriver.Firefox(executable_path="C:\Python27\wires.exe")
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
+        time.sleep(2)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -80,7 +84,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(2)
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
